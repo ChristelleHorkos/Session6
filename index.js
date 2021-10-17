56a9d7a9f1f0b057a3ea290e506c758cec5f3aac
Christelle Horkos <christellehorkos22@gmail.com>
2021-10-17 11:09:02 +0300

    7thChange

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ function test()
 {
     fetch('https://protected-stream-92531.herokuapp.com/data')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(json =>{
+      var ul=document.getElementById("ul01")
+   json.forEach(element => {
+       var li=document.createElement("li")
+       li.innerHTML=element.name
+       ul.appendchild(li)
+   });
+    
+    
+    
+  })
 }
